b088ac43f017734bc285f2d9a3ace4cd3ccf04be
Amir Aghighi <amiraghighi.work@gmail.com>
2024-06-21 10:59:32 +0330

    feat: add pint and reverb

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Process;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('pint', function () {
+    $result = Process::run('./vendor/bin/pint');
+    $this->info($result->output());
+
+    return 0;
+});
